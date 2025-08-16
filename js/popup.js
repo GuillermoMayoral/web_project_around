db90776e633ff9609f97a5415993e1511eaf9e08
Mayoral Guillermo <g.mayoral.m@outlook.com>
2025-08-16 14:44:55 -0600

    I finish js functions about popup and hovers wiht haert function active

---
+++ js/popup.js
@@ -0,0 +1,37 @@
+//llamar popup
+let btneEditButton = document.querySelector(".profile__edit-button");
+let popUpBlock = document.querySelector(".popup");
+//cerrar popup
+let btnClosePopUp = document.querySelector(".popup__close");
+//cambiar nombre y descripcion
+let profileName = document.querySelector(".profile__name");
+let profileDescription = document.querySelector(".profile__description");
+let inputName = document.querySelector(".popup__name-input");
+let inputDescription = document.querySelector(".popup__description-input");
+let btnChangeName = document.querySelector(".popup__button");
+
+function openPopUp(e) {
+    e.preventDefault();
+    popUpBlock.classList.remove("popup--disabled");
+}
+
+function closePopUp(e) {
+    e.preventDefault();
+    popUpBlock.classList.add("popup--disabled");
+}
+
+function editName(e) {
+    e.preventDefault();
+    if (inputName.value == "" || inputDescription.value == "") {
+        alert("Un campo esta vacio");
+    } else {
+        flag = 0;
+        profileName.textContent = inputName.value;
+        profileDescription.textContent = inputDescription.value;
+        closePopUp(e);
+    }
+}
+
+btneEditButton.addEventListener("click", openPopUp);
+btnClosePopUp.addEventListener("click", closePopUp);
+btnChangeName.addEventListener("click", editName);
